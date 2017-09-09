77bf0c7de91885dcc672158b4225500367144469
d1a9x8u9 <daxu7803@gmail.com>
2017-09-09 11:00:57 -0700

    Import future

--- cs4660/tutorial/lists.py
+++ cs4660/tutorial/lists.py
@@ -1,5 +1,5 @@
 """Lists defines simple list related operations"""
-__future__ import division
+from __future__ import division
 
 def get_first_item(li):
     """Return the first item from the list"""
