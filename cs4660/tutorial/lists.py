4afc4d561ebfd1902ffc30c6c67bb60cbfa14a51
d1a9x8u9 <daxu7803@gmail.com>
2017-09-13 10:30:23 -0700

    Add graph related code from course repo

--- cs4660/tutorial/lists.py
+++ cs4660/tutorial/lists.py
@@ -1,29 +1,21 @@
 """Lists defines simple list related operations"""
-from __future__ import division
 
 def get_first_item(li):
     """Return the first item from the list"""
-    x = li[0]
-    return x
+    pass
 
 def get_last_item(li):
     """Return the last item from the list"""
-    x = li[-1]
-    return x
+    pass
 
 def get_second_and_third_items(li):
     """Return second and third item from the list"""
-    x = li[1:3]
-    return x
+    pass
 
 def get_sum(li):
     """Return the sum of the list items"""
-    x = sum(li)
-    return x
+    pass
 
 def get_avg(li):
     """Returns the average of the list items"""
-    x = sum(li)
-    y = len(li)
-    return x/y
-    
+    pass
